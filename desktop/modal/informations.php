72c489c0d8173e84d14285ff85725404cf44860e
Jean-François Chalony <jean-francois.chalony@liksi.fr>
2020-02-17 08:34:23 +0100

    search bug

--- desktop/modal/informations.php
+++ desktop/modal/informations.php
@@ -12,7 +12,7 @@
 	echo '<tr><td colspan="2" style="background-color:#555">Equipement '.$eqLogic->getName().'</td></tr>';
 
     log::add('Palazzetti', 'debug','affichage des infos');
-	$value = json_decode($eqLogic->getCmd('info','ITime')->getCache()['value']);
+	/*$value = json_decode($eqLogic->getCmd('info','ITime')->getCache()['value']);
     log::add('Palazzetti', 'debug','Itime: '. $value);
 	echo '<tr><td>Heure du poêle</td><td>'. $value->STOVE_DATETIME.'</td></tr>';
 	echo '<tr><td>Jour de la semaine</td><td>'. Palazzetti::getWeekDay($value->STOVE_WDAY).'</td></tr>';
@@ -95,6 +95,7 @@
 	echo '<tr><td>Canal</td><td>'. $value->WCH.'</td></tr>';
 	echo '<tr><td>SSID</td><td>'. $value->WSSID.'</td></tr>';
 	echo '<tr><td>Type de cryptage</td><td>'. $value->WENC.'</td></tr>';
+	*/
 ?>
 	</tbody>
 </table>
